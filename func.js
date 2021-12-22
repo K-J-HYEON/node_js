48046df928aaa0f2a40b3bdb0b8b23034f19712b
K-J-HYEON <kjh950601@naver.com>
2021-12-23 00:20:22 +0900

    node_js

--- func.js
+++ func.js
@@ -1,7 +1,2 @@
-const example = { a: 123, b: {c: 135, d: 146} }
-const a = example.a;
-const d = example.b.d;
-
-const { a, b:{ d } } = example;
-console.log(a);
-console.log(d);
+const value = require('./var');
+console.log(value);
