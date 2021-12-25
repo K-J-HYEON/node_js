262e066e7294e4d6189616e06dae64e798e335c9
K-J-HYEON <kjh950601@naver.com>
2021-12-25 21:59:17 +0900

    nodeJS

--- worker_threads.js
+++ worker_threads.js
@@ -1,10 +1,20 @@
 const { Worker, isMainThread, parentPort} = require('worker_threads');
 
 if(isMainThread) { // 메인쓰레드
-    const worker = new Worker(__filename);
-    worker.on('message', (value) => console.log('워커로부터', value));
-    worker.on('exit', () => console.log('워커 끝~'));
-    worker.postMessage('ping');
+    const threads = new Set();
+    threads.add(new Worker(__filename, {
+        workerData: {start: 1},
+    }));
+    threads.add(new Worker(__filename, {
+        workerData: {start: 2},
+    }));
+    for (let worker of threads) {
+        worker.on('message', (value) => console.log('워커로부터', value));
+        worker.on('exit', () => {
+            console.log('워커 끝~');
+        });
+    }
+
 } else { // 워커쓰레드
     parentPort.on('message', (value) => {
         console.log('부모로부터', value);
